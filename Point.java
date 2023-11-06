707bb4863fd63fbc4d55b049ec87921d97c19405
Faris hackerman <farissharayri@gmail.com>
2023-11-06 17:06:24 +0300

    entity creation

---
+++ Point.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class Point {
+    private int x_coordinate;
+    private int y_coordinate;
+
+
+    public Point(){
+
+
+    }
+
+    public void setX_coordinate(int x_coordinate) {
+        this.x_coordinate = x_coordinate;
+    }
+
+    public int getX_coordinate() {
+        return x_coordinate;
+    }
+
+    public void setY_coordinate(int y_coordinate) {
+        this.y_coordinate = y_coordinate;
+    }
+
+    public int getY_coordinate() {
+        return y_coordinate;
+    }
+}
